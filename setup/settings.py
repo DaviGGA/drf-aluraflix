4d1a2b2ee9c32d0730c9e9c25087590eafdff335
DaviGGA <davigregorio.a@hotmail.com>
2022-11-16 16:53:26 -0300

    Debug=False

--- setup/settings.py
+++ setup/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='hbd#gu&n7ecjn94=_q)r-!#o#r_l!-jnlx@pg2+zl+qa63nyo3')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
